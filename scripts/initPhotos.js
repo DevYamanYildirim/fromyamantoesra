d017b549f4df92650c741676469a8cc4840c7240
Yaman <yamanyildirim@Mac.vodafone.ultrahub>
2026-02-14 02:07:49 +0000

    first commit from VSCode

--- scripts/initPhotos.js
+++ scripts/initPhotos.js
@@ -4,9 +4,19 @@ function initPhotos() {
   const photoCards = document.querySelectorAll('.photo-card');
 
   const images = [
-    './images/photo1.jpg',
-    './images/photo2.jpg',
-    './images/photo3.jpg',
+    './images/esra-1.jpg',
+    './images/esra-2.jpg',
+    './images/esra-3.jpg',
+    './images/esra-4.jpg',
+    './images/esra-5.jpg',
+    './images/esra-6.jpg',
+    './images/esra-7.jpg',
+    './images/esra-8.jpg',
+    './images/esra-9.jpg',
+    './images/esra-10.jpg',
+    './images/esra-11.jpg',
+    './images/esra-12.jpg',
+    './images/esra-13.jpg',
   ];
 
   photoCards.forEach((card) => {
